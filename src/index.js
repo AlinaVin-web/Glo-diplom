7d942e862a765e91afc87021807ab290d1cc1027
Alina Vinogradova <ellafoss@yandex.ru>
2024-11-22 00:35:30 +0300

    Открытие оригиналов документов

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import callModals from "./modules/call-modals";
 import scroll from "./modules/scroll"
+import documents from "./modules/documents";
 
 callModals();
 scroll();
+documents();
---
+++ src/modules/documents.js
@@ -0,0 +1,50 @@
+import { animate } from "./helpers.js";
+
+const documents = () => {
+    const docs = document.querySelectorAll('a.sertificate-document');
+
+    docs.forEach(doc => {
+        doc.addEventListener('click', (e) => {
+            e.preventDefault();
+
+            const originDoc = document.createElement('div');
+            const originImg = document.createElement('img');
+            originImg.src = doc.href;
+            originImg.alt = doc.href.match(/[^\\\/]*$/)[0];
+            originDoc.className = "origin-document";
+            originDoc.append(originImg);
+            document.body.append(originDoc);
+
+            if (screen.width >= 768) {
+                animate({
+                    duration: 150,
+                    timing(timeFraction) {
+                        return timeFraction;
+                    },
+                    draw(progress) {
+                        originImg.style = `transform: translateY(-${30 - progress * 30}%);`;
+                        originDoc.style = `opacity: ${progress};`
+                    }
+                });
+            }
+
+            originDoc.addEventListener('click', () => {
+                if (screen.width >= 768) {
+                    animate({
+                        duration: 150,
+                        timing(timeFraction) {
+                            return timeFraction;
+                        },
+                        draw(progress) {
+                            originImg.style = `transform: translateY(-${progress * 30}%);`;
+                            originDoc.style = `opacity: ${1 - progress};`
+                        }
+                    });
+                }
+                setTimeout(() => originDoc.remove(), 150);
+            });
+        });
+    });
+}
+
+export default documents;
